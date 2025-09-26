c8ed7fddb2569033344adfcb5d44f4decd9b1880
Ilya Degtyarenko <is.degtyarenko@gmail.com>
2025-09-26 19:24:54 +0300

    feat: Add TailwindCSS, Sass, and base styles

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import './assets/styles/css/libs.css'
+import './assets/styles/sass/main.sass'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ vue() ],
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
   resolve: { // https://vite.dev/config/shared-options.html
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
