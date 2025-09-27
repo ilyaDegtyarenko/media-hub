e0cd45dd5e2e97b8ab44fc49b1b8b5d942d5d85e
Ilya Degtyarenko <is.degtyarenko@gmail.com>
2025-09-27 22:15:43 +0300

    feat: Integrate Pinia for state management and update app initialization

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import './assets/styles/css/libs.css'
 import './assets/styles/sass/main.sass'
+import '@/assets/styles/css/libs.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './plugins/router'
+import App from '@/App.vue'
+import pinia from '@/plugins/pinia'
+import router from '@/plugins/router'
 
 const app = createApp(App)
 
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
